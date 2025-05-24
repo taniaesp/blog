4ec71729819435eaf498885908e1f3e016e38cdc
Tania Espinoza <estephaniae.12@gmail.com>
2025-05-23 18:49:20 -0600

    Añadiendo funcionalidad al formulario y entradas

--- Project/index.js
+++ Project/index.js
@@ -4,7 +4,46 @@ document.addEventListener('DOMContentLoaded', () => {
     const formulario = document.getElementById('blog-form');
     const entradasDiv = document.getElementById('entradas');
 
-    let entradas = JSON.parse(localStorage.getItem(entradas)) || [];
+    let entradas = JSON.parse(localStorage.getItem('entradas')) || [];
+
+    function guardarEntradas () {
+        localStorage.setItem('entradas', JSON.stringify(entradas));
+    }
+
+    function renderEntradas() {
+        entradasDiv.innerHTML = '';  //Limpiar antes de renderizar
+
+        entradas.forEach((entrada, index) => {
+            const entradaDiv = document.createElement('div');
+            entradaDiv.classList.add('entrada');
+            entradaDiv.setAttribute('data-id', index);
+
+            entradaDiv.innerHTML = `
+            <h2>${entrada.titulo}</h2>
+            <p>${entrada.contenido}</p>
+            <div class = "entrada-buttons">
+                <button class = "edit-btn"> Edit </button>
+                <button class = "delete-btn"> Delete </button> 
+            </div>
+            `;
+
+            entradasDiv.prepend(entradaDiv); //Mostrar la mas reciente arriba
+        });
+    }
+
+    formulario.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const titulo = document.getElementById('titulo').value;
+        const contenido = document.getElementById('contenido').value;
+
+        const nuevaEntrada ={ titulo, contenido};
+        entradas.push(nuevaEntrada);
+        guardarEntradas();
+        renderEntradas();
+        formulario.reset();
+    });
+
 
     
 })
